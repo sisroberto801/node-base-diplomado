5b63616f3a640a75c2feded31ec60b9a0079f5eb
rolguin <ext_roberto.olguin@cajalosandes.cl>
2026-03-08 10:44:35 -0400

    actualizacion de swagger

---
+++ src/config/swagger.js
@@ -0,0 +1,35 @@
+import swaggerJSDoc from 'swagger-jsdoc';
+import swaggerUi from 'swagger-ui-express';
+import env from './env.js';
+
+const options = {
+  definition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'API con Express',
+      version: '1.0.0',
+      description: 'Documentación de la API (Swagger + Express)',
+    },
+    servers: [
+      {
+        url: 'http://localhost:3000',
+      },
+    ],
+  },
+  apis: [
+    './src/docs/project.swagger.yaml',
+    './src/controllers/*.js',
+    './src/routes/*.js',
+  ],
+};
+
+const swaggerSpec = swaggerJSDoc(options);
+
+export const swaggerDocs = (app, port) => {
+  app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+
+  app.get('/api-docs.json', (req, res) => {
+    res.setHeader('Content-Type', 'application/json');
+    res.send(swaggerSpec);
+  });
+};
